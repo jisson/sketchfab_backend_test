3ab64b2d677ceb653e66522fb29e1ebd66657fef
pierre <pierre.rodier@gmail.com>
2016-11-05 15:50:18 +0100

    Refactored templates structure

--- sketchfab/models.py
+++ sketchfab/models.py
@@ -1,13 +1,4 @@
-from django.contrib.auth.models import User
 from django.db import models
-
-
-class UserProfile(models.Model):
-
-    user = models.ForeignKey(User, unique=True)
-
-    # Badges
-    # badges
 
 
 class Model3d(models.Model):
